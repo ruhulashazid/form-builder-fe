0d5a6118e0403a0ab23d80d4cb2b535b816aa74f
ruhulashazid <ruhulaminshazid@gmail.com>
2024-10-24 12:45:13 +0600

    path set

--- src/utils/SocketManager.ts
+++ src/utils/SocketManager.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // initializing the socket connection
-let socket = io("https://gfc-be-7a05iea80-ruhul-amins-projects-750c43e0.vercel.app/");
+let socket = io("https://gfc-be-7a05iea80-ruhul-amins-projects-750c43e0.vercel.app");
 
 export default socket;
